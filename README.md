9d9131e89a5b3419b77adf5bbb6a9283270cfca7
Tomohiro Kumagai <kumagai@sr-co.co.jp>
2018-08-15 16:22:55 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Basic Lesson
+PythonでAWS Lambda実装するための事前トレーニング
+
+[tutorial](https://www.pythonweb.jp/tutorial)
---
+++ start_python.py
@@ -0,0 +1,10 @@
+#coding: UTF-8
+import sys
+
+print(u"こんにちわ")
+
+# 標準入力、標準出力、標準エラー出力
+print(sys.stdin.encoding)
+print(sys.stdout.encoding)
+print(sys.stderr.encoding)
+
